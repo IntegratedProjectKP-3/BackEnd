c77982ec8a8a2d14805df142a6b31bb3006dc456
Thanakorn-kmutt <111366471+Thanakorn-kmutt@users.noreply.github.com>
2024-05-14 07:22:50 +0700

    fixing_not_working_yet

--- Backend-main/src/main/java/com/itbangmodkradankanbanapi/exception/GlobalExceptionHandler.java
+++ Backend-main/src/main/java/com/itbangmodkradankanbanapi/exception/GlobalExceptionHandler.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,6 +16,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorResponse> handleItemNotFoundException(ItemNotFoundException ex, HttpServletRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(Timestamp.from(Instant.now()), HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(ItemNotFoundForUpdateAndDelete.class)
+    public ResponseEntity<ItemNotFoundResponse> handleItemNotFoundException(ItemNotFoundForUpdateAndDelete ex, HttpServletRequest request) {
+        ItemNotFoundResponse errorResponse = new  ItemNotFoundResponse(Timestamp.from(Instant.now()), HttpStatus.NOT_FOUND.value(), ex.getMessage() , request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
--- Backend-main/src/main/java/com/itbangmodkradankanbanapi/exception/ItemNotFoundForUpdateAndDelete.java
+++ Backend-main/src/main/java/com/itbangmodkradankanbanapi/exception/ItemNotFoundForUpdateAndDelete.java
@@ -3,13 +3,11 @@ package com.itbangmodkradankanbanapi.exception;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ItemNotFoundException extends RuntimeException{
-    public ItemNotFoundException(String message){
+public class ItemNotFoundForUpdateAndDelete extends RuntimeException{
+    public ItemNotFoundForUpdateAndDelete(String message){
         super(message);
     }
 }
